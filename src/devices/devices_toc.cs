9bb082aa35395b1b4e18fd647969887e2e6f832a
Clay Murphy <claym@google.com>
2016-07-08 13:33:37 -0700

    Docs: Add Block Phone Numbers N release document
    Bug: 27444215
    Change-Id: I4aef2eca9e33ecf0bba08092ba1993613b0d6ffb

--- src/devices/devices_toc.cs
+++ src/devices/devices_toc.cs
@@ -298,6 +298,7 @@
             </a>
         </div>
         <ul>
+          <li><a href="<?cs var:toroot ?>devices/tech/connect/block-numbers.html">Block Phone Numbers</a></li>
           <li><a href="<?cs var:toroot ?>devices/tech/connect/felica.html">Host Card Emulation of FeliCa</a></li>
         </ul>
       </li>
